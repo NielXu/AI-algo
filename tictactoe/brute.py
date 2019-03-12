10d6267ca058cda23923a832ef7ea003ff772e7e
Daniel Xu <niellxu@yahoo.ca>
2019-03-12 00:35:21 -0400

    Remake game, added information for each algorithm

---
+++ tictactoe/brute.py
@@ -0,0 +1,15 @@
+"""
+Using brute force algorithm, list out all possible moves and
+always takes the best move. This algorithm is OK for this small
+game since there are only limited amount of possibilities. But
+when it comes to big game such as chess, it will take forever to
+decide the next move.
+
+Wiki:
+In computer science, brute-force search or exhaustive search,
+also known as generate and test, is a very general problem-solving
+technique and algorithmic paradigm that consists of systematically
+enumerating all possible candidates for the solution and checking
+whether each candidate satisfies the problem's statement.
+"""
+
--- tictactoe/game.py
+++ tictactoe/game.py
@@ -1,94 +1,99 @@
 """
-Using Minimax algorithm to make the best moves in
-game tic-tac-toe.
+Classic tic tac toe game, does not handle any kinds
+of errors.
 """
-class Game():
-    def __init__(self):
-        self.board = [["-" for x in range(3)] for x in range(3)]
-        self.flag = 1
-    
-    def show(self):
-        "Show the game board"
-        for i in self.board:
-            print(i)
-    
-    def move(self):
-        "Return 1 if it is player move, 0 if computer move"
-        return self.flag
-    
-    def place(self, x, y):
-        "Make move, select (x,y)"
-        if self.flag == 1:
-            self.flag = 0
-            self.board[x][y] = "O"
+from copy import deepcopy
+
+
+def init_board():
+    "Initializing a board and return it as a nested list"
+    return [["-" for x in range(3)] for x in range(3)]
+
+
+def make_move(board, x, y, p):
+    "Make move, set O if it is player or X if it is computer at (x,y)"
+    board[x][y] = "O" if p == True else "X"
+
+
+def check(board):
+    """Check if there is any winner on the board, return 1 if computer win,
+    0 if player win, None if no winner
+    """
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0] != '-':
+            return 1 if board[i][0] == "X" else 0
+        if board[0][i] == board[1][i] == board[2][i] and board[0][i] != '-':
+            return 1 if board[0][i] == "X" else 0
+        if board[0][0] == board[1][1] == board[2][2] and board[0][0] != '-':
+            return 1 if board[0][0] == "X" else 0
+        if board[0][2] == board[1][1] == board[2][0] and board[0][2] != '-':
+            return 1 if board[0][2] == "X" else 0
+
+
+def is_full(board):
+    "Check if board is full"
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == '-':
+                return False
+    return True
+
+
+def is_placed(board, x, y):
+    "Check if (x,y) had been placed or not"
+    return board[x][y] != '-'
+
+
+def cp_board(board):
+    "Make a copy of the board without changing the original one"
+    return deepcopy(board)
+
+def show(board):
+    "Print board nicely"
+    for i in range(3):
+        row = ''
+        for j in range(3):
+            row += board[i][j]
+            if j != 2:
+                row += ' | '
+        print(row)
+        if i != 2:
+            print("----------")
+
+
+def game():
+    b = init_board()
+    p = True
+    while True:
+        show(b)
+        if p:
+            x,y = input("Eneter your move(Player):").split(",")
+            make_move(b, int(x), int(y), p)
+            p = False
         else:
-            self.flag = 1
-            self.board[x][y] = "X"
-    
-    def end(self):
-        "Check if player or computer win, return None if no one wins"
-        for i in range(3):
-            if self.board[i][0] == self.board[i][1] == self.board[i][2] and self.board[i][0] != '-':
-                return 0 if self.board[i][0] == "X" else 1
-            if self.board[0][i] == self.board[1][i] == self.board[2][i] and self.board[0][i] != '-':
-                return 0 if self.board[0][i] == "X" else 1
-        if self.board[0][0] == self.board[1][1] == self.board[2][2] and self.board[0][0] != '-':
-            return 0 if self.board[0][0] == "X" else 1
-        if self.board[0][2] == self.board[1][1] == self.board[2][0] and self.board[0][2] != '-':
-            return 0 if self.board[0][2] == "X" else 1
-    
-    def isfull(self):
-        "Check if the board is full already"
-        for i in range(3):
-            for j in range(3):
-                if self.board[i][j] == "-":
-                    return False
-        return True
-    
-    def reset(self):
-        self.board = [["-" for x in range(3)] for x in range(3)]
-        self.flag = 1
+            x,y = input("Eneter your move(Computer)").split(",")
+            make_move(b, int(x), int(y), p)
+            p = True
+        result = check(b)
+        if result == 0:
+            print("Congraz, u win!")
+            break
+        elif result == 1:
+            print("Sorry, computer win")
+            break
+        else:
+            if is_full(b):
+                print("Draw")
+                break
+    show(b)
 
 
 def start():
-    game = Game()
-    flag = True
-    game.show()
-    while flag:
-        result = game.end()
-        if result is not None:
-            if result == 1:
-                print("GameOver, Player win!")
-                flag = restart(game)
-            else:
-                print("GameOver, Computer win!")
-                flag = restart(game)
-        else:
-            if game.isfull():
-                print("GameOver, Draw!")
-                flag = restart(game)
-            else:
-                rnd = game.move()
-                if rnd == 1:
-                    mv = input("Eneter your move:")
-                    x,y = mv.split(",")
-                    game.place(int(x), int(y))
-                else:
-                    mv = input("Computer move:")
-                    x,y = mv.split(",")
-                    game.place(int(x), int(y))
-                game.show()
+    while True:
+        game()
+        again = input("Do you wanna try again?(y/n)")
+        if again == 'n':
+            break
 
-def restart(game):
-    again = input("Do you wanna try again?(y/n)")
-    if again == "y":
-        print("=============")
-        print("=============")
-        print("=============")
-        game.reset()
-        game.show()
-        return True
-    else:
-        return False
 
 start()
